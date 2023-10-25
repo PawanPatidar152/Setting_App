b9e88b981c431d9a7c8dcb42c034cca9fccd2948
Pawan Patidar <rkpawanpatidar12345@gmail.com>
2023-10-25 17:32:32 +0530

    deleted Setting App folder

--- Setting App/src/ReusableComponent/TextBwnMainComponant.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './TextBwnMainComponant.css'
-function TextBwnMainComponant(props) {
-  return (
-   <p id='text' >{props.text}</p>
-  )
-}
-
-export default TextBwnMainComponant
